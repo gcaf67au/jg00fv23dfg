e3552db3a5ac314a2ff4e295da7ebc9ea620ed20
jugfd <bilqissaraswati@gmail.com>
2016-09-20 05:37:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" data-form-id="">
-  <head>
 <head>
-	<title>BEST LIBRARY</title>
-	</head>
+<title>Online LIBRARY</title>
+</head>
 	<body style="margin:0px;padding:0px;overflow:hidden">
-	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/stellar/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
-
-    <?php include('asite.php');?>
-    <?php include('asite-pdf.php');?>
-  	<?php include('asite-doc.php');?>
-  	<?php include('asite-rtf.php');?>
-	</body>
-
+	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/tessellate/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
+  <?php include('asite-doc.php');?>
+  <?php include('asite-pdf.php');?>
+  <?php include('asite-rtf.php');?>
+  <?php include('asite.php');?>
+</body>  
 </html>
