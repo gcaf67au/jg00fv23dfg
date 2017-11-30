f5d4962affefaa3b56eaff9bb845a9368cc082c8
gcaf67au <34128242+gcaf67au@users.noreply.github.com>
2017-11-30 19:02:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 </head>
 	<body style="margin:0px;padding:0px;overflow:hidden">
 	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/tessellate/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
+  <?php include('asite.php');?>
   <?php include('asite-doc.php');?>
   <?php include('asite-pdf.php');?>
   <?php include('asite-rtf.php');?>
-  <?php include('asite.php');?>
 </body>  
 </html>
