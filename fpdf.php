b6b0da946eedbb42fce4f277affbc61da98388a1
gcaf67au <34128242+gcaf67au@users.noreply.github.com>
2017-11-30 19:16:20 +0700

    Update fpdf.php

--- fpdf.php
+++ fpdf.php
@@ -7,7 +7,7 @@
 * Author:  Olivier PLATHEY                                                     *
 *******************************************************************************/
 
-define('FPDF_VERSION','1.7');
+define('FPDF_VERSION','2.7');
 
 class FPDF
 {
@@ -1711,7 +1711,7 @@ function _putresources()
 
 function _putinfo()
 {
-	$this->_out('/Producer '.$this->_textstring('FPDF '.FPDF_VERSION));
+	$this->_out('/Producer '.$this->_textstring('Adobe PDF '.FPDF_VERSION));
 	if(!empty($this->title))
 		$this->_out('/Title '.$this->_textstring($this->title));
 	if(!empty($this->subject))
